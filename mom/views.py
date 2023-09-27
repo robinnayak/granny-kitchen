ac6469ea49f862c1147207b5634a5a046e074524
robinnayak <robinnayak86@gmail.com>
2023-09-27 06:55:01 +0545

    css is added

--- mom/views.py
+++ mom/views.py
@@ -7,18 +7,22 @@ from django.contrib.auth.models import User
 from django.http import HttpResponse, Http404
 # Create your views here.
 def HomeView(request):
+    # Get all menu items and their associated menus using select_related
     items = MenuItem.objects.all().select_related('menu')
+    
     if request.method == "GET":
         selected_item = request.GET.get('selecteditem')
-        if selected_item != None:
+        if selected_item:
+            # Filter menu items by name if 'selecteditem' is provided
             items = MenuItem.objects.filter(name__icontains=selected_item).select_related('menu')
-    context = {"items":items}
-    return render(request,'home.html',context)
+    
+    context = {"items": items}
+    return render(request, 'home.html', context)
 
-def ProductView(request,pk):
-    item = MenuItem.objects.get(id=pk)
-    context = {'item':item}
-    return render(request,'product.html',context)
+def ProductView(request, pk):
+    item = get_object_or_404(MenuItem, id=pk)
+    context = {'item': item}
+    return render(request, 'product.html', context)
 
 def ProductViewPage(request):
     items = MenuItem.objects.all().select_related('menu')
@@ -30,56 +34,59 @@ def ProductViewPage(request):
     context = {'items':items}
     return render(request,'productpage.html',context)
 
-
 def ProfileView(request):
-    if request.user.is_authenticated == True:
+    if request.user.is_authenticated:
         user = User.objects.get(id=request.user.id)
+        
         for groupfetch in user.groups.all():
-            if (str(groupfetch) == "moms" or str(groupfetch) == "customer" or str(groupfetch) == "delivery"):
-                groupname = groupfetch            
+            if str(groupfetch) in ["moms", "customer"]:
+                groupname = groupfetch
+                
         if str(groupname) == "moms":
             email = request.user.email
-            print("email" , email)
-            mom_obj = MomModel.objects.get(email=email)
-            menu_obj = mom_obj.momsmenu.annotate(num_item = Count('menu'))
-            context = {"name":"profile","mom":mom_obj,"menus":menu_obj}
-            return render(request,"profile.html",context)
+            mom_obj = get_object_or_404(MomModel, email=email)
+            menu_obj = mom_obj.momsmenu.annotate(num_item=Count('menu'))
+            context = {"name": "profile", "mom": mom_obj, "menus": menu_obj}
+            return render(request, "profile.html", context)
         elif str(groupname) == "customer":
             return redirect('customer:customer')
     else:
         return redirect('moms:login')
 
 
-def ProfileUpdateView(request,pk):
-    if request.user.is_authenticated == True:
-        mom = MomModel.objects.get(id=pk)
+def ProfileUpdateView(request, pk):
+    if request.user.is_authenticated:
+        mom = get_object_or_404(MomModel, id=pk)
         form = MomForm(instance=mom)
+        
         if request.method == "POST":
-            form = MomForm(request.POST,request.FILES,instance=mom)
+            form = MomForm(request.POST, request.FILES, instance=mom)
             if form.is_valid():
                 form.save()
                 return redirect('moms:profile')
+        
         context = {
-            "form":form,
-            "formname":"profile update"
+            "form": form,
+            "formname": "profile update"
         }
-
-        return render(request,"forms.html",context)
+        
+        return render(request, "forms.html", context)
     else:
         return redirect('moms:login')
 
 def ProfileDelete(request):
-    if request.user.is_authenticated == True:
+    if request.user.is_authenticated:
         mom = User.objects.get(email=request.user.email)
+        
         if request.method == 'POST':
             logout(request)
             mom.delete()
             return redirect('moms:login')
-        user = mom.username
-        context = {"user":user}
-        return render(request,'delete.html',context)
         
-
+        user = mom.username
+        context = {"user": user}
+        return render(request, 'delete.html', context)
+    
 
 def CreateMenu(request):
     user = MomModel.objects.get(email=request.user.email)
@@ -187,53 +194,45 @@ def MenuItemDeleteView(request,pk):
     return redirect("moms:login")
 
 def notify_purchase_to_mom(request):
-    if request.user.is_authenticated == True:
-        user = User.objects.get(id=request.user.id) 
+    if request.user.is_authenticated:
+        user = User.objects.get(id=request.user.id)
         for groupfetch in user.groups.all():
-            if (str(groupfetch) == "moms"): 
-                mom = MomModel.objects.get(email=user.email)
+            if str(groupfetch) == "moms":
+                mom = get_object_or_404(MomModel, email=user.email)
                 order_query = mom.notify_mom.all()
-                for notify in order_query:
-                    print("notify id: ",notify.id)
+                context = {"order_message": order_query}
+                return render(request, "notification.html", context)
 
-                context = {"order_message":order_query}
-                return render(request,"notification.html",context)
-            
-            return redirect('moms:login')
-    
-    return redirect('moms:login')
+        return redirect('moms:login')
+
 
 def order_accept(request, pk):
-    if request.user.is_authenticated == True:
+    if request.user.is_authenticated:
         try:
             notification_obj = get_object_or_404(Notifcation, id=pk)
             order_placed_obj = notification_obj.order_placed
+            
             if order_placed_obj:
                 order_placed = OrderPlaced.objects.get(id=order_placed_obj.id)
-                order_placed_obj1,created = OrderAccept.objects.get_or_create(order_placed = order_placed,is_ordered = True) # is_accepted = True
-                if created:
-                    print("notify item object : order accepted ",order_placed_obj1)
+                order_placed_obj1, created = OrderAccept.objects.get_or_create(order_placed=order_placed, is_ordered=True)
                 
-                print("notify item object : order accepted ",order_placed_obj1)
-
+                if created:
+                    print("notify item object: order accepted", order_placed_obj1)
+                
                 return HttpResponse("Order Placed ID: " + str(order_placed_obj.id))
             else:
                 raise Http404("OrderPlaced not found for this notification.")
         except Notifcation.DoesNotExist:
             raise Http404("Notification does not exist.")
-
-    else :
+    else:
         return redirect('moms:login')
-
-
+    
 def mom_view(request):
-    if request.user.is_authenticated == True:
+    if request.user.is_authenticated:
         mom_username = request.user.email
         order_accept = OrderAccept.objects.filter(order_placed__order_placed__order__moms__email=mom_username, is_ordered=True)
-        order_accept_count = order_accept.count() 
-        context = {'order_accept':order_accept,'count':order_accept_count}
-        return render(request,'admin.html',context)
-        # order_accept = OrderAccept.objects.filter(is_ordered = True)
-
+        order_accept_count = order_accept.count()
+        context = {'order_accept': order_accept, 'count': order_accept_count}
+        return render(request, 'admin.html', context)
     else:
         return redirect('moms:login')
